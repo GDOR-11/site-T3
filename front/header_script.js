0b07f24615261d4a025194fa02f6cb4b3804be22
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2026-04-29 16:53:46 -0300

    feature: mudei o header dependendo se o usuario estiver logado ou nao

--- front/header_script.js
+++ front/header_script.js
@@ -1,7 +1,16 @@
-const username = await (await fetch("/api/auth/getuser")).text();
-
-if (username !== "") {
-    document.getElementById("unauthenticated-auth-section").style.display = "none";
-    document.getElementById("authenticated-auth-section").style.display = "flex";
-    document.getElementById("header-username").innerText = username;
+function createLink(text, href) {
+    const a = document.createElement("a");
+    a.innerText = text;
+    a.href = href;
+    return a;
 }
+
+fetch("/api/auth/getuser").then(response => response.text()).then(username => {
+    if (username !== "") {
+        document.getElementById("header-username").innerText = username;
+        document.querySelector("div.dropdown-content > [href=\"/login\"]").innerText = "Trocar conta";
+        document.querySelector("div.dropdown-content > [href=\"/register\"]").remove();
+        document.querySelector("div.dropdown-content").append(createLink("Notas", "/notas"));
+        document.querySelector("div.dropdown-content").append(createLink("Faltas", "/faltas"));
+    }
+});
